28a9991ebeb2ac16dced93655ebd11fa86e05044
MahmoudShalma <mahmoud.shalma@outlook.com>
2021-06-15 23:34:51 +0200

    lab 3

---
+++ lab 3/index.js
@@ -0,0 +1,6 @@
+
+const app = require('./src/app')
+const PORT = 3500
+app.listen(PORT, ()=>{
+    console.log(`we are live on localhost:${PORT}`)
+})
---
+++ lab 3/src/app.js
@@ -0,0 +1,26 @@
+
+const express = require('express')
+const hbs = require('hbs')
+const path = require('path')
+
+const app=express()
+app.set('view engine', 'hbs')
+
+const viewsDir = path.join(__dirname, '../resources/views')
+const layoutsDir = path.join(__dirname, '../resources/layouts')
+
+app.set('views', viewsDir)
+hbs.registerPartials(layoutsDir)
+
+
+app.get('', (req,res)=>{
+    res.render('index')
+})
+
+
+const taskContoller = require('../controllers/taskApp.controller')
+app.get('/addTaskForm', taskContoller.addTaskController)
+app.get('/showAllTasks',taskContoller.showAllTasksController)
+app.get('/delete/:id',taskContoller.deleteSingleTask)
+app.get('/edit/:id',taskContoller.editSingleTask)
+module.exports = app
